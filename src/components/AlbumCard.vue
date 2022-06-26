22c8193c68f10be1a8a63adaa3acdc766c9d28a6
FernandoSilvera <fernando.silvera02@gmail.com>
2022-06-26 17:13:41 +0200

    Project Bootstrap, album card created.

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="card">
+      <img :src="album.cover" :alt="`${album.title} cover`">
+      <div class="card-body">
+        <h5 class="card-title">{{ album.title }}</h5>
+        <h6 class="card-subtitle">{{ album.author }}</h6>
+        <h6 class="card-subtitle">{{ album.year }}</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumCard",
+  props: {
+    album: Object,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: rgb(38, 49, 59);
+  
+    img {
+    object-fit: cover;
+  }
+}
+</style>
