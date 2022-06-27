db1537f1244a7e154abfe1dee5f7c6c9674566d9
FernandoSilvera <fernando.silvera02@gmail.com>
2022-06-27 16:44:01 +0200

    Albums list created.

---
+++ src/components/AlbumData.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="card">
+      <img :src="album.poster" :alt="`${album.title} cover`">
+      <div class="card-body">
+        <h5 class="card-title text-light">{{ album.title }}</h5>
+        <h6 class="card-subtitle text-secondary">{{ album.author }}</h6>
+        <h6 class="card-subtitle text-secondary">{{ album.year }}</h6>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AlbumData",
+  props: {
+    album: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  background-color: rgb(38, 49, 59);
+  
+    img {
+    object-fit: cover;
+  }
+}
+</style>
---
+++ src/components/AlbumsList.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row row-cols-5">
+        <div class="col" v-for="(album, i) in albumsList" :key="i">
+          <AlbumData :album="album"></AlbumData>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+import AlbumData from "./AlbumData.vue";
+
+export default {
+  name: "AlbumsList",
+  components: { AlbumData },
+  data() {
+    return {
+      urlAlbums: "https://flynn.boolean.careers/exercises/api/array/music",
+      albumsList: [],
+    }
+  },
+  methods: {
+    fetchAlbumsData() {
+      axios
+      .get(this.urlAlbums)
+      .then((resp) => {
+        console.log(resp.data);
+        this.albumsList = resp.data.response;
+      })
+    }
+  },
+  mounted() {
+    this.fetchAlbumsData();
+  },
+
+}
+</script>
+
+<style>
+
+</style>
